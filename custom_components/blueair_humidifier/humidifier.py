d2adae14b2549ddc1661d56b0bfbd305a2d54a87
Jon Ryfetten <jon.ryfetten@outlook.com>
2022-07-14 00:39:15 +0200

    Bug fix

--- custom_components/blueair_humidifier/humidifier.py
+++ custom_components/blueair_humidifier/humidifier.py
@@ -350,7 +350,7 @@ class BlueairAirPurifier(HumidifierEntity):
  #   _LOGGER.warning(GetSwitchbotDevices().get_bots())
     #bot: Switchbot = Switchbot(mac="c0:fd:37:e2:2f:ad")
    # _LOGGER.warning(bot)
-    return await self.next_state(from_state, None)
+    return await self.next_state(from_state)
 
   async def next_state(self, from_state: str) -> bool:
     _LOGGER.warning('Set_ mode from ' + from_state + " to " + self.next_mode)
