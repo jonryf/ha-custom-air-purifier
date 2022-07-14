f7094906fdad017bd211b1e846c3c8db3bfbf2e7
Jon Ryfetten <jon.ryfetten@outlook.com>
2022-07-14 21:06:10 +0200

    Add delay

--- custom_components/blueair_humidifier/humidifier.py
+++ custom_components/blueair_humidifier/humidifier.py
@@ -308,6 +308,9 @@ class BlueairAirPurifier(HumidifierEntity):
   def step(self, count=0):
     _LOGGER.warning("sleep " + str(count))
 
+    if time.time() - self.last_press < 1.5:
+      time.sleep(1.5-(time.time() - self.last_press))
+
     if time.time() - self.last_press < 2.5:
       # switch state
       
